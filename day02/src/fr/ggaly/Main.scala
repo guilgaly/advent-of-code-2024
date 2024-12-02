e128301b2a6b372f708c3f961cf6328000210252
Guillaume Galy <guilgaly@gmail.com>
2024-12-02 12:11:25 +0100

    Day 2 - part 1

--- day02/src/fr/ggaly/Main.scala
+++ day02/src/fr/ggaly/Main.scala
@@ -2,13 +2,32 @@ package fr.ggaly
 
 @main def main(): Unit =
   val input = Input.readLines
+  val reports = input.map(line => line.split(' ').map(_.toInt).toList)
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(reports)}")
 
   println(s"Part 2 result: ${part2()}")
 
-def part1(): Int =
-  0
+def part1(reports: List[List[Int]]): Int =
+  reports.count(report => safelyIncreasing(report) || safelyDecreasing(report))
 
 def part2(): Int =
   0
+
+private def safelyIncreasing(report: List[Int]): Boolean =
+  report
+    .sliding(2)
+    .foldLeft(true) {
+      case (false, _)                => false
+      case (true, List(left, right)) => right > left && (right - left) <= 3
+      case _                         => throw IllegalArgumentException()
+    }
+
+private def safelyDecreasing(report: List[Int]): Boolean =
+  report
+    .sliding(2)
+    .foldLeft(true) {
+      case (false, _)                => false
+      case (true, List(left, right)) => left > right && (left - right) <= 3
+      case _                         => throw IllegalArgumentException()
+    }
--- day02/test/src/fr/ggaly/MainSuite.scala
+++ day02/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,18 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val reports = List(
+    List(7, 6, 4, 2, 1),
+    List(1, 2, 7, 8, 9),
+    List(9, 7, 6, 2, 1),
+    List(1, 3, 2, 4, 5),
+    List(8, 6, 4, 4, 1),
+    List(1, 3, 6, 7, 9),
+  )
+
+  test("part 1") {
+    assertEquals(part1(reports), 2)
+  }
 
   test("part 2") {}
+end MainSuite
