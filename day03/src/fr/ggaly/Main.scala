2d6c671fecbcb1220e43336be952e01d3deafc35
Guillaume Galy <guilgaly@gmail.com>
2024-12-03 09:28:27 +0100

    Day 3 - part 1

--- day03/src/fr/ggaly/Main.scala
+++ day03/src/fr/ggaly/Main.scala
@@ -3,12 +3,17 @@ package fr.ggaly
 @main def main(): Unit =
   val input = Input.readLines
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(input.mkString("\n"))}")
 
   println(s"Part 2 result: ${part2()}")
 
-def part1(): Int =
-  0
+def part1(input: String): Int =
+  val regex = """mul\(([0-9]+),([0-9]+)\)""".r
+  regex
+    .findAllIn(input)
+    .matchData
+    .map(r => r.group(1).toInt * r.group(2).toInt)
+    .sum
 
 def part2(): Int =
   0
--- day03/test/src/fr/ggaly/MainSuite.scala
+++ day03/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,12 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val program =
+    "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+  test("part 1") {
+    assertEquals(part1(program), 161)
+  }
 
   test("part 2") {}
+end MainSuite
