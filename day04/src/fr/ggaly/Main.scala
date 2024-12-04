907f9811fa68c202a18adaeed4a714705d437e0a
Guillaume Galy <guilgaly@gmail.com>
2024-12-04 10:25:27 +0100

    Day 4

--- day04/src/fr/ggaly/Main.scala
+++ day04/src/fr/ggaly/Main.scala
@@ -1,14 +1,82 @@
 package fr.ggaly
 
 @main def main(): Unit =
-  val input = Input.readLines
+  val input = Input.readLines.map(line => line.toCharArray).toArray
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(input)}")
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 2 result: ${part2(input)}")
 
-def part1(): Int =
-  0
+def part1(input: Array[Array[Char]]): Int =
+  val maxY = input.length - 1
+  val maxX = input(0).length - 1
 
-def part2(): Int =
-  0
+  var count = 0
+  for
+    y <- 0 to maxY
+    x <- 0 to maxX
+  do
+    if input(y)(x) == 'X' then
+      // Horizontal
+      if x <= maxX - 3 && input(y)(x + 1) == 'M' && input(y)(
+          x + 2,
+        ) == 'A' && input(y)(x + 3) == 'S'
+      then count += 1
+      if x >= 3 && input(y)(x - 1) == 'M' && input(y)(x - 2) == 'A' && input(y)(
+          x - 3,
+        ) == 'S'
+      then count += 1
+      // Vertical
+      if y <= maxY - 3 && input(y + 1)(x) == 'M' && input(y + 2)(
+          x,
+        ) == 'A' && input(y + 3)(x) == 'S'
+      then count += 1
+      if y >= 3 && input(y - 1)(x) == 'M' && input(y - 2)(x) == 'A' && input(
+          y - 3,
+        )(x) == 'S'
+      then count += 1
+      // Diagonal
+      if x <= maxX - 3 && y <= maxY - 3 && input(y + 1)(x + 1) == 'M' && input(
+          y + 2,
+        )(x + 2) == 'A' && input(y + 3)(x + 3) == 'S'
+      then count += 1
+      if x <= maxX - 3 && y >= 3 && input(y - 1)(x + 1) == 'M' && input(y - 2)(
+          x + 2,
+        ) == 'A' && input(y - 3)(x + 3) == 'S'
+      then count += 1
+      if x >= 3 && y <= maxY - 3 && input(y + 1)(x - 1) == 'M' && input(y + 2)(
+          x - 2,
+        ) == 'A' && input(y + 3)(x - 3) == 'S'
+      then count += 1
+      if x >= 3 && y >= 3 && input(y - 1)(x - 1) == 'M' && input(y - 2)(
+          x - 2,
+        ) == 'A' && input(y - 3)(x - 3) == 'S'
+      then count += 1
+    end if
+  end for
+
+  count
+end part1
+
+def part2(input: Array[Array[Char]]): Int =
+  val maxY = input.length - 1
+  val maxX = input(0).length - 1
+
+  var count = 0
+  for
+    y <- 1 until maxY
+    x <- 1 until maxX
+  do
+    if input(y)(x) == 'A' then
+      val topLeft = input(y - 1)(x - 1)
+      val downLeft = input(y + 1)(x - 1)
+      val topRight = input(y - 1)(x + 1)
+      val downRight = input(y + 1)(x + 1)
+
+      if ((topLeft == 'M' && downRight == 'S') || (topLeft == 'S' && downRight == 'M')) && ((downLeft == 'M' && topRight == 'S') || (downLeft == 'S' && topRight == 'M'))
+      then count += 1
+    end if
+  end for
+
+  count
+end part2
--- day04/test/src/fr/ggaly/MainSuite.scala
+++ day04/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,24 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val testInput = Array(
+    "MMMSXXMASM".toCharArray,
+    "MSAMXMSMSA".toCharArray,
+    "AMXSXMAAMM".toCharArray,
+    "MSAMASMSMX".toCharArray,
+    "XMASAMXAMM".toCharArray,
+    "XXAMMXXAMA".toCharArray,
+    "SMSMSASXSS".toCharArray,
+    "SAXAMASAAA".toCharArray,
+    "MAMMMXMMMM".toCharArray,
+    "MXMXAXMASX".toCharArray,
+  )
 
-  test("part 2") {}
+  test("part 1") {
+    assertEquals(part1(testInput), 18)
+  }
+
+  test("part 2") {
+    assertEquals(part2(testInput), 9)
+  }
+end MainSuite
