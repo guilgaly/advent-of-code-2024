59eb5c81546e8c5bd7485b25fbb20b9e84c8ca55
Guillaume Galy <guilgaly@gmail.com>
2024-12-05 11:35:26 +0100

    Day 5

--- day05/src/fr/ggaly/Main.scala
+++ day05/src/fr/ggaly/Main.scala
@@ -1,14 +1,59 @@
 package fr.ggaly
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 @main def main(): Unit =
-  val input = Input.readLines()
+  val rules = Input
+    .readLines("rules.txt")
+    .map(_.split('|').map(_.toInt) match
+      case Array(left, right) => (left, right),
+    )
+  val updates =
+    Input.readLines("updates.txt").map(_.split(',').map(_.toInt).toList)
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(rules, updates)}")
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 2 result: ${part2(rules, updates)}")
+end main
 
-def part1(): Int =
-  0
+def part1(rules: List[(Int, Int)], updates: List[List[Int]]): Int =
+  updates
+    .filter(isUpdateValid(rules))
+    .map(update => update(update.length / 2))
+    .sum
 
-def part2(): Int =
-  0
+def part2(rules: List[(Int, Int)], updates: List[List[Int]]): Int =
+  val isValid = isUpdateValid(rules)
+
+  @tailrec
+  def recursiveReOrder(update: List[Int]): List[Int] =
+    if isValid(update) then update
+    else
+      val reordered = update.to(mutable.ListBuffer)
+      for case (left, right) <- rules do
+        val leftIndex = reordered.indexOf(left)
+        val rightIndex = reordered.indexOf(right)
+        if leftIndex >= 0 && rightIndex >= 0 && rightIndex < leftIndex then
+          val removed = reordered.remove(leftIndex)
+          reordered.insert(rightIndex, removed)
+      // Keep applying until all rules are satisfied together
+      recursiveReOrder(reordered.toList)
+    end if
+  end recursiveReOrder
+
+  updates
+    .filter(!isValid(_))
+    .map { update =>
+      val reordered = recursiveReOrder(update)
+      reordered(reordered.length / 2)
+    }
+    .sum
+end part2
+
+private def isUpdateValid(rules: List[(Int, Int)])(update: List[Int]): Boolean =
+  rules.forall { case (left, right) =>
+    val leftIndex = update.indexOf(left)
+    val rightIndex = update.indexOf(right)
+    leftIndex < 0 || rightIndex < 0 || leftIndex < rightIndex
+  }
--- day05/test/src/fr/ggaly/MainSuite.scala
+++ day05/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,43 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val rules = List(
+    (47, 53),
+    (97, 13),
+    (97, 61),
+    (97, 47),
+    (75, 29),
+    (61, 13),
+    (75, 53),
+    (29, 13),
+    (97, 29),
+    (53, 29),
+    (61, 53),
+    (97, 53),
+    (61, 29),
+    (47, 13),
+    (75, 47),
+    (97, 75),
+    (47, 61),
+    (75, 61),
+    (47, 29),
+    (75, 13),
+    (53, 13),
+  )
+  private val updates = List(
+    List(75, 47, 61, 53, 29),
+    List(97, 61, 53, 29, 13),
+    List(75, 29, 13),
+    List(75, 97, 47, 61, 53),
+    List(61, 13, 29),
+    List(97, 13, 75, 29, 47),
+  )
 
-  test("part 2") {}
+  test("part 1") {
+    assertEquals(part1(rules, updates), 143)
+  }
+
+  test("part 2") {
+    assertEquals(part2(rules, updates), 123)
+  }
+end MainSuite
