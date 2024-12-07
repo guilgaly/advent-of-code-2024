b62022f12407a8dc6e487693140fb474f4a978a8
Guillaume Galy <guilgaly@gmail.com>
2024-12-07 22:47:35 +0100

    Day 7

--- day07/src/fr/ggaly/Main.scala
+++ day07/src/fr/ggaly/Main.scala
@@ -1,14 +1,43 @@
 package fr.ggaly
 
 @main def main(): Unit =
-  val input = Input.readLines()
+  val input = Input.readLines().map { line =>
+    val s = line.split(": ")
+    val testValue = s(0).toLong
+    val numbers = s(1).split(' ').map(_.toLong).toList
+    Equation(testValue, numbers)
+  }
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(input)}")
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 2 result: ${part2(input)}")
+end main
 
-def part1(): Int =
-  0
+case class Equation(testValue: Long, numbers: List[Long])
 
-def part2(): Int =
-  0
+def part1(input: List[Equation]): Long =
+  calibration(input, (a, b) => List(a + b, a * b))
+
+def part2(input: List[Equation]): Long =
+  calibration(input, (a, b) => List(a + b, a * b, concat(a, b)))
+
+private def calibration(
+    input: List[Equation],
+    operations: (a: Long, b: Long) => List[Long],
+) =
+  input
+    .filter {
+      case Equation(testValue, first :: rest) =>
+        rest
+          .foldLeft(List(first))((acc, next) =>
+            acc.flatMap(a => operations(a, next)).filter(_ <= testValue),
+          )
+          .contains(testValue)
+      case _ =>
+        throw new IllegalStateException()
+    }
+    .map(_.testValue)
+    .sum
+
+private def concat(a: Long, b: Long): Long =
+  (a.toString + b.toString).toLong
--- day07/test/src/fr/ggaly/MainSuite.scala
+++ day07/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,23 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val testInput = List(
+    Equation(190, List(10, 19)),
+    Equation(3267, List(81, 40, 27)),
+    Equation(83, List(17, 5)),
+    Equation(156, List(15, 6)),
+    Equation(7290, List(6, 8, 6, 15)),
+    Equation(161011, List(16, 10, 13)),
+    Equation(192, List(17, 8, 14)),
+    Equation(21037, List(9, 7, 18, 13)),
+    Equation(292, List(11, 6, 16, 20)),
+  )
 
-  test("part 2") {}
+  test("part 1") {
+    assertEquals(part1(testInput), 3749L)
+  }
+
+  test("part 2") {
+    assertEquals(part2(testInput), 11387L)
+  }
+end MainSuite
