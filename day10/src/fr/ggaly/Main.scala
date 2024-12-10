b189992dd56d68c8584800455ddf9e0357cbde4f
Guillaume Galy <guilgaly@gmail.com>
2024-12-10 13:58:16 +0100

    Day 10

--- day10/src/fr/ggaly/Main.scala
+++ day10/src/fr/ggaly/Main.scala
@@ -1,14 +1,47 @@
 package fr.ggaly
 
 @main def main(): Unit =
-  val input = Input.readLines()
+  val input = TrailMap(Input.readLines().map(_.map(_.asDigit).toArray).toArray)
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(input)}")
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 2 result: ${part2(input)}")
 
-def part1(): Int =
-  0
+def part1(trailMap: TrailMap): Int =
+  def findTrailEnds(currentPoint: Point): Set[Point] =
+    if trailMap.height(currentPoint) == 9 then Set(currentPoint)
+    else trailMap.nextOnTrail(currentPoint).toSet.flatMap(findTrailEnds)
+  end findTrailEnds
 
-def part2(): Int =
-  0
+  trailMap.trailHeads.map(findTrailEnds(_).size).sum
+
+def part2(trailMap: TrailMap): Int =
+  def countTrails(currentPoint: Point): Int =
+    if trailMap.height(currentPoint) == 9 then 1
+    else trailMap.nextOnTrail(currentPoint).map(countTrails).sum
+  end countTrails
+
+  trailMap.trailHeads.map(countTrails).sum
+
+final private case class Point(x: Int, y: Int)
+
+final private case class TrailMap(map: Array[Array[Int]]):
+  private val xMax: Int = map(0).length - 1
+  private val yMax: Int = map.length - 1
+  val trailHeads: List[Point] = map.iterator.zipWithIndex
+    .flatMap((line, y) =>
+      line.zipWithIndex.collect { case (0, x) => Point(x, y) },
+    )
+    .toList
+  def height(p: Point): Int =
+    map(p.y)(p.x)
+  def heightOpt(p: Point): Option[Int] =
+    val Point(x, y) = p
+    if x >= 0 && x <= xMax && y >= 0 && y <= yMax then Some(height(p))
+    else None
+  def nextOnTrail(p: Point): List[Point] =
+    val Point(x, y) = p
+    Iterator(Point(x - 1, y), Point(x + 1, y), Point(x, y - 1), Point(x, y + 1))
+      .filter(neighbour => heightOpt(neighbour).contains(height(p) + 1))
+      .toList
+end TrailMap
--- day10/test/src/fr/ggaly/MainSuite.scala
+++ day10/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,24 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val testInput = TrailMap(
+    Array(
+      Array(8, 9, 0, 1, 0, 1, 2, 3),
+      Array(7, 8, 1, 2, 1, 8, 7, 4),
+      Array(8, 7, 4, 3, 0, 9, 6, 5),
+      Array(9, 6, 5, 4, 9, 8, 7, 4),
+      Array(4, 5, 6, 7, 8, 9, 0, 3),
+      Array(3, 2, 0, 1, 9, 0, 1, 2),
+      Array(0, 1, 3, 2, 9, 8, 0, 1),
+      Array(1, 0, 4, 5, 6, 7, 3, 2),
+    ),
+  )
 
-  test("part 2") {}
+  test("part 1") {
+    assertEquals(part1(testInput), 36)
+  }
+
+  test("part 2") {
+    assertEquals(part2(testInput), 81)
+  }
+end MainSuite
