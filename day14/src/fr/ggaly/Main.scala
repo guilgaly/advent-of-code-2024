0063a5bf285c5743eca3582be1b7c5ef6af554bf
Guillaume Galy <guilgaly@gmail.com>
2024-12-16 11:03:44 +0100

    Day 14

--- day14/src/fr/ggaly/Main.scala
+++ day14/src/fr/ggaly/Main.scala
@@ -1,14 +1,126 @@
 package fr.ggaly
 
+import java.io.PrintStream
+import java.nio.file.{Files, Path, Paths}
+import scala.annotation.targetName
+import scala.util.Using
+
 @main def main(): Unit =
-  val input = Input.readLines()
+  val input = parseInput(Input.readLines())
 
-  println(s"Part 1 result: ${part1()}")
+  val space = Space(101, 103)
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 1 result: ${part1(space, input)}")
 
-def part1(): Int =
-  0
+  println(s"Part 2 result: ${part2(space, input)}")
+end main
 
-def part2(): Int =
-  0
+def part1(space: Space, input: List[Robot]): Int =
+  val endState = (0 until 100).foldLeft(input) { (robots, i) =>
+    robots.map { case Robot(position, speed) =>
+      Robot(space.move(position, speed), speed)
+    }
+  }
+  space.safetyFactor(endState)
+
+def part2(space: Space, input: List[Robot]): Int =
+  Files.createDirectories(Paths.get("day14/print1"))
+  Files.createDirectories(Paths.get("day14/print2"))
+  Files.createDirectories(Paths.get("day14/print3"))
+
+  def next(robots: List[Robot]): List[Robot] =
+    robots.map { case Robot(position, speed) =>
+      Robot(space.move(position, speed), speed)
+    }
+
+  def printMap(dir: Path, robots: List[Robot], time: Int): Unit =
+    Using(new PrintStream(Files.newOutputStream(dir.resolve(s"$time.txt")))) { ps =>
+      space.printMap(robots, ps)
+    }.get
+
+  val dir1 = Paths.get("day14/print1")
+  Files.createDirectories(dir1)
+  (1 to 1_000).foldLeft(input) { (robots, i) =>
+    val newRobots = next(robots)
+    printMap(dir1, newRobots, i)
+    newRobots
+  }
+
+  // Look through the printed files, notice two cycles where the outputs look non random; print only these cycles
+  // When both cycles match, we get a christmas tree.
+
+  val dir2 = Paths.get("day14/print2")
+  Files.createDirectories(dir2)
+  val dir3 = Paths.get("day14/print3")
+  Files.createDirectories(dir3)
+  val dir4 = Paths.get("day14/christmas_trees")
+  Files.createDirectories(dir4)
+
+  var response = 0
+  (1 to 10_000).foldLeft(input) { (robots, i) =>
+    val newRobots = next(robots)
+    if (i - 4) % 103 == 0 then printMap(dir2, newRobots, i)
+    if (i - 29) % 101 == 0 then printMap(dir3, newRobots, i)
+    if (i - 4) % 103 == 0 && (i - 29) % 101 == 0 then
+      printMap(dir4, newRobots, i)
+      if response == 0 then response = i
+    newRobots
+  }
+
+  response
+end part2
+
+def parseInput(lines: List[String]): List[Robot] =
+  val regex = """p=(\d+),(\d+) v=(-?\d+),(-?\d+)""".r
+  lines.map { line =>
+    val matched = regex
+      .findFirstMatchIn(line)
+      .getOrElse(throw IllegalArgumentException(s"Unparsable line $line"))
+    Robot(
+      Coords(matched.group(1).toLong, matched.group(2).toLong),
+      Coords(matched.group(3).toLong, matched.group(4).toLong),
+    )
+  }
+end parseInput
+
+final case class Coords(x: Long, y: Long):
+  @targetName("add")
+  def +(vector: Coords): Coords = Coords(x + vector.x, y + vector.y)
+  @targetName("multiply")
+  def *(mult: Long): Coords = Coords(x * mult, y * mult)
+
+final case class Robot(position: Coords, speed: Coords)
+
+final case class Space(width: Long, height: Long):
+  private val xMid = width / 2
+  private val yMid = height / 2
+
+  def move(point: Coords, vector: Coords): Coords =
+    val p = point + vector
+    val x = if p.x >= width then p.x - width else if p.x < 0 then width + p.x else p.x
+    val y = if p.y >= height then p.y - height else if p.y < 0 then height + p.y else p.y
+    Coords(x, y)
+
+  def safetyFactor(robots: List[Robot]): Int =
+    val (tl, tr, bl, br) =
+      robots.foldLeft((0, 0, 0, 0)) {
+        case ((topLeft, topRight, bottomLeft, bottomRight), Robot(Coords(x, y), _)) =>
+          if x < xMid && y < yMid then (topLeft + 1, topRight, bottomLeft, bottomRight)
+          else if x > xMid && y < yMid then (topLeft, topRight + 1, bottomLeft, bottomRight)
+          else if x < xMid && y > yMid then (topLeft, topRight, bottomLeft + 1, bottomRight)
+          else if x > xMid && y > yMid then (topLeft, topRight, bottomLeft, bottomRight + 1)
+          else (topLeft, topRight, bottomLeft, bottomRight)
+      }
+    tl * tr * bl * br
+  end safetyFactor
+
+  def printMap(robots: List[Robot], o: PrintStream): Unit =
+    val robotsMap = robots.groupMapReduce(_.position)(_ => 1)(_ + _)
+    for y <- 0L until height do
+      for x <- 0L until width do
+        robotsMap.get(Coords(x, y)) match
+          case Some(count) => o.print(count)
+          case None        => o.print(".")
+      o.println()
+  end printMap
+end Space
--- day14/test/src/fr/ggaly/MainSuite.scala
+++ day14/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,28 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val testInput = parseInput(
+    List(
+      "p=0,4 v=3,-3",
+      "p=6,3 v=-1,-3",
+      "p=10,3 v=-1,2",
+      "p=2,0 v=2,-1",
+      "p=0,0 v=1,3",
+      "p=3,0 v=-2,-2",
+      "p=7,6 v=-1,-3",
+      "p=3,0 v=-1,-2",
+      "p=9,3 v=2,3",
+      "p=7,3 v=-1,2",
+      "p=2,4 v=2,-3",
+      "p=9,5 v=-3,-3",
+    ),
+  )
+
+  private val testSpace = Space(11, 7)
+
+  test("part 1") {
+    assertEquals(part1(testSpace, testInput), 12)
+  }
 
   test("part 2") {}
+end MainSuite
