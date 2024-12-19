5ce34218cb64822cc24db5c6d4ca277d37beb3dd
Guillaume Galy <guilgaly@gmail.com>
2024-12-19 11:18:01 +0100

    Day 19

--- day19/src/fr/ggaly/Main.scala
+++ day19/src/fr/ggaly/Main.scala
@@ -1,14 +1,35 @@
 package fr.ggaly
 
+import scala.collection.mutable
+
 @main def main(): Unit =
-  val input = Input.readLines()
+  val towels = Input.read("towels.txt").split(',').map(_.trim).toList
+  val designs = Input.readLines("designs.txt")
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(towels, designs)}")
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 2 result: ${part2(towels, designs)}")
 
-def part1(): Int =
-  0
+def part1(towels: List[String], designs: List[String]): Int =
+  def isValid(design: String): Boolean =
+    design.isEmpty || towels.exists(t => design.startsWith(t) && isValid(design.drop(t.length)))
 
-def part2(): Int =
-  0
+  designs.count(isValid)
+
+def part2(towels: List[String], designs: List[String]): Long =
+  val cache = mutable.HashMap.empty[String, Long]
+
+  def countArrangements(design: String): Long =
+    if design.isEmpty then 1L
+    else
+      cache.get(design) match
+        case Some(count) => count
+        case _ =>
+          val count = towels.collect {
+            case t if design.startsWith(t) => countArrangements(design.drop(t.length))
+          }.sum
+          cache.addOne(design -> count)
+          count
+
+  designs.map(countArrangements).sum
+end part2
--- day19/test/src/fr/ggaly/MainSuite.scala
+++ day19/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,23 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  private val testTowels = List("r", "wr", "b", "g", "bwu", "rb", "gb", "br")
+  private val testPatterns = List(
+    "brwrr",
+    "bggr",
+    "gbbr",
+    "rrbgbr",
+    "ubwu",
+    "bwurrg",
+    "brgr",
+    "bbrgwb",
+  )
 
-  test("part 2") {}
+  test("part 1") {
+    assertEquals(part1(testTowels, testPatterns), 6)
+  }
+
+  test("part 2") {
+    assertEquals(part2(testTowels, testPatterns), 16L)
+  }
+end MainSuite
