07a6cd42571370fd621b68e1e484bc41ac0e93ea
Guillaume Galy <guilgaly@gmail.com>
2024-12-22 13:42:44 +0100

    Day 22

--- day22/src/fr/ggaly/Main.scala
+++ day22/src/fr/ggaly/Main.scala
@@ -1,14 +1,60 @@
 package fr.ggaly
 
+import scala.collection.mutable
+
 @main def main(): Unit =
-  val input = Input.readLines()
+  val input = Input.readLines().map(_.toLong)
 
-  println(s"Part 1 result: ${part1()}")
+  println(s"Part 1 result: ${part1(input)}")
 
-  println(s"Part 2 result: ${part2()}")
+  println(s"Part 2 result: ${part2(input)}")
 
-def part1(): Int =
-  0
+def part1(input: List[Long]): Long =
+  input.map(initSecret => (1 to 2000).foldLeft(initSecret)((acc, _) => nextSecret(acc))).sum
 
-def part2(): Int =
-  0
+def part2(input: List[Long]): Int =
+  val changesToPriceMaps =
+    input.map { initSecret =>
+      val prices = (1 to 2000)
+        .foldLeft((initSecret, List(price(initSecret)))) { case ((secret, prices), _) =>
+          val ns = nextSecret(secret)
+          (ns, price(ns) :: prices)
+        }
+        ._2
+        .reverse
+
+      prices
+        .sliding(5)
+        .map { window =>
+          val List(x0, x1, x2, x3, x4) = window
+          (x1 - x0, x2 - x1, x3 - x2, x4 - x3) -> x4
+        }
+        .foldLeft(Map.empty[(Int, Int, Int, Int), Int]) { case (acc, (changes, price)) =>
+          acc.get(changes) match
+            case Some(existingPrice) => acc
+            case None                => acc + (changes -> price)
+        }
+    }
+
+  val changesToTotalPrice = mutable.HashMap.empty[(Int, Int, Int, Int), Int]
+  changesToPriceMaps.foreach(_.foreach { case (k, v) =>
+    changesToTotalPrice.updateWith(k) {
+      case Some(existingValue) => Some(existingValue + v)
+      case None                => Some(v)
+    }
+  })
+
+  changesToTotalPrice.values.max
+end part2
+
+def price(secret: Long): Int =
+  secret.toString.last.asDigit
+
+def nextSecret(prev: Long): Long =
+  val step1 = prune(mix(prev, prev * 64L))
+  val step2 = prune(mix(step1, step1 / 32L))
+  prune(mix(step2, step2 * 2048L))
+
+def mix(a: Long, b: Long): Long = a ^ b
+
+def prune(a: Long) = a % 16777216
--- day22/test/src/fr/ggaly/MainSuite.scala
+++ day22/test/src/fr/ggaly/MainSuite.scala
@@ -3,6 +3,20 @@ package fr.ggaly
 import munit.FunSuite
 
 class MainSuite extends FunSuite:
-  test("part 1") {}
+  test("primitives") {
+    assertEquals(mix(42L, 15L), 37L)
+    assertEquals(prune(100000000L), 16113920L)
+    assertEquals(nextSecret(123L), 15887950L)
+    assertEquals(nextSecret(15887950L), 16495136L)
+    assertEquals(nextSecret(16495136L), 527345L)
+    assertEquals(nextSecret(527345L), 704524L)
+  }
 
-  test("part 2") {}
+  test("part 1") {
+    assertEquals(part1(List(1L, 10L, 100L, 2024L)), 37327623L)
+  }
+
+  test("part 2") {
+    assertEquals(part2(List(1L, 2L, 3L, 2024L)), 23)
+  }
+end MainSuite
